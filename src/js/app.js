7e1540820ee0da07dd0500f3ffd200e4449d864e
Redheady <iam@redheady.ru>
2022-08-23 20:53:53 +0300

    Update intro slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,10 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper from 'swiper/bundle';
 import '../scss/app.scss';
-import 'swiper/css';
 import 'swiper/css/bundle';
 /* Your JS Code goes here */
 
-new Swiper ('.swiper-slider', {
-    modules: [Navigation, Pagination],
+new Swiper('.swiper-slider', {
+    effect: "fade",
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
