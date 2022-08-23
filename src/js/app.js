542dadfef0607946f5ad04f1ebed87b0b4f3e87c
unknown <delight_1@mail.ru>
2022-08-23 20:56:18 +0500

    Главная

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,19 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
 import '../scss/app.scss';
-
+import 'swiper/css';
+import 'swiper/css/bundle';
 /* Your JS Code goes here */
 
-
+new Swiper ('.swiper-slider', {
+    modules: [Navigation, Pagination],
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    spaceBetween: 1000,
+    loop: true,
+    autoplay: {
+        delay: 1000,
+    },
+    speed: 800,
+});
