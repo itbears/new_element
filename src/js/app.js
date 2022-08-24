1ad757c423bb97045fedb3100affe60ed5ed390d
unknown <delight_1@mail.ru>
2022-08-24 14:34:45 +0500

    Шапка

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,10 @@
 import Swiper, { Navigation, Pagination } from 'swiper';
 import '../scss/app.scss';
-import 'swiper/css';
+// import 'swiper/css';
 import 'swiper/css/bundle';
 /* Your JS Code goes here */
 
 new Swiper ('.swiper-slider', {
-    modules: [Navigation, Pagination],
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -17,3 +16,22 @@ new Swiper ('.swiper-slider', {
     },
     speed: 800,
 });
+
+// sublist  
+let menu = document.querySelector('.header__user-city-item');
+let subMenu = document.querySelector('.header__user-city-item-sublist');
+
+menu.addEventListener('click', function() {
+    subMenu.classList.toggle('display-block');
+} )
+
+//header-catalog
+let catalogBtn = document.querySelector('.header__user-button');
+let headerCatalog = document.querySelector('.header__catalog');
+
+catalogBtn.addEventListener('click', function() {
+    headerCatalog.classList.toggle('catalog-block');
+} )
+// catalogBtn.addEventListener('click', function() {
+//     headerCatalog.classList.toggle('header__catalog--top');
+// })
