7fcaec143aebc568ac143058eb8edcff0982f6a3
Redheady <iam@redheady.ru>
2022-09-03 19:54:06 +0300

    A: Update header fix

--- src/js/app.js
+++ src/js/app.js
@@ -19,13 +19,13 @@ new Swiper('.swiper-slider', {
     speed: 800,
 });
 
-// sublist  
-let menu = document.querySelector('.header__user-city-item');
-let subMenu = document.querySelector('.header__user-city-item-sublist');
+// // sublist  
+// let menu = document.querySelector('.header__user-city-item');
+// let subMenu = document.querySelector('.header__user-city-item-sublist');
 
-menu.addEventListener('click', function () {
-    subMenu.classList.toggle('display-block');
-})
+// menu.addEventListener('click', function () {
+//     subMenu.classList.toggle('display-block');
+// })
 
 //header-catalog
 let catalogBtn = document.querySelector('.header__user-button');
@@ -40,10 +40,19 @@ let cityBtn = document.querySelector('.header__user-city');
 let citySubList = document.querySelector('.header__user-city-item-sublist');
 let cityIcon = document.querySelector('.header__user-city-icon');
 let citySvg = document.querySelector('.header__user-city-svg');
+let subMenu = document.querySelector('.header__user-city-item-sublist');
 
 cityBtn.addEventListener('click', function () {
     cityBtn.classList.toggle('city-active');
     citySubList.classList.toggle('sublist-active');
     cityIcon.classList.toggle('display-none');
     citySvg.classList.toggle('transform');
+    subMenu.classList.toggle('display-block');
+})
+
+const headerBurger = document.querySelector(".header__burger")
+const headerNav = document.querySelector(".header__nav")
+headerBurger.addEventListener("click", () => {
+    headerBurger.classList.toggle("active")
+    headerNav.classList.toggle("active")
 })
